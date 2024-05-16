2207126b1fc37441812e1edec27abae0aa114713
PatrickViscan <168373938+PatrickViscan@users.noreply.github.com>
2024-05-16 22:22:37 +0200

    Update install.sh

--- .github/workflows/mac/install.sh
+++ .github/workflows/mac/install.sh
@@ -4,4 +4,4 @@ set -e
 
 ccache -M 200M
 
-CC="clang" CXX="clang++" CFLAGS="-Werror" CXXFLAGS="-Werror" CMAKE_BUILD_TYPE=RelWithDebInfo ./travis/install.sh
+CC="clang" CXX="clang++" CFLAGS="" CXXFLAGS="" CMAKE_BUILD_TYPE=RelWithDebInfo ./travis/install.sh
